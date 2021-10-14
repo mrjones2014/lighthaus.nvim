3fb624a8aba7017904133bed62c4b7c6d820334e
Mat Jones <mat@mjones.network>
2021-10-14 09:02:17 -0400

    Revert "tweak lualine theme"
    This reverts commit fadd50043b4107b0668479b7d20694c9be0ad214.

--- lua/lighthaus/utils.lua
+++ lua/lighthaus/utils.lua
@@ -36,7 +36,8 @@ function M.lighten(hex, amount, fg)
   return M.blend(hex, fg or M.fg, math.abs(amount))
 end
 
-function M.tint_lualine_color(color, colors)
+function M.tint_lualine_color(color)
+  local colors = require('lighthaus.colors')
   local group = {
     a = { bg = color, fg = colors.bg },
     b = { bg = M.darken(color, 0.2), fg = M.lighten(color, 0.2) },
--- lua/lualine/themes/lighthaus.lua
+++ lua/lualine/themes/lighthaus.lua
@@ -2,11 +2,11 @@ local util = require('lighthaus.utils')
 local colors = require('lighthaus.colors')
 
 local lualine_colors = {
-  normal = util.tint_lualine_color(colors.blue, colors),
-  insert = util.tint_lualine_color(colors.green, colors),
-  command = util.tint_lualine_color(colors.purple, colors),
-  visual = util.tint_lualine_color(colors.yellow, colors),
-  replace = util.tint_lualine_color(colors.red, colors),
+  normal = util.tint_lualine_color(colors.blue),
+  insert = util.tint_lualine_color(colors.green),
+  command = util.tint_lualine_color(colors.purple),
+  visual = util.tint_lualine_color(colors.yellow),
+  replace = util.tint_lualine_color(colors.red),
   inactive = {
     a = { bg = colors.bg, fg = colors.fg_nc_statusline },
     b = { bg = colors.bg, fg = colors.fg_nc_statusline },
